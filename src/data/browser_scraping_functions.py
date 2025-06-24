c8688d4b1c527cdb66fcaea5d837f478e0f6f1ba
philjhowson <philjhowson@outlook.com>
2025-06-24 18:22:35 +0200

    updating src files

--- src/data/browser_scraping_functions.py
+++ src/data/browser_scraping_functions.py
@@ -3,14 +3,35 @@ import random
 import time
 
 def human_sleep(min_sec = 3.0, max_sec = 5.0):
+    """
+    Simple function to simulate how people will periodically stop
+    scrolling to look at something. Randomized value.
+    args:
+        min_sec: minimum possible sleep time.
+        max_sec: maximum possible sleep time.
+    """
     time.sleep(np.random.uniform(min_sec, max_sec))
 
 def random_mouse_move(page):
+    """
+    Moves the mouse to a random x,y position using a non-straight
+    line.
+    args:
+        page: the page object being used by playwright.
+    """
     x = random.randint(0, 800)
     y = random.randint(0, 600)
     page.mouse.move(x, y, steps = random.randint(5, 15))
 
 def random_mouse_hover(page, target):
+    """
+    Moves the mouse to hover over an object on a page, designed
+    to simulate how people may be curious about certain links, posts,
+    and users on social media.
+    args:
+        page: page object used by playwright.
+        target: the target to hover over.
+    """
 
     selector = f'a[href="{target}"]'
     
@@ -32,6 +53,18 @@ def random_mouse_hover(page, target):
 
 
 def open_random_link(target, base_url, context):
+    """
+    Opens a randomized link from the page in order to simulate
+    curiosity that people will have when scrolling. It will spend
+    a small amount of time looking at the picture, if an image was
+    loaded, if it is a page, it will scroll down a bit before closing
+    the tab.
+    args:
+        target: target link to open.
+        base_url: the base url of the page, the threads page in this case
+        context: the original context opened with playwrite such that this
+                 will simulate opening a new tab, not a new window.
+    """
 
     url = f"{base_url}{target}"
 
@@ -42,9 +75,9 @@ def open_random_link(target, base_url, context):
         human_sleep(2, 4)  
 
         if target.endswith('/media'):
-            human_sleep(10, 15)
+            human_sleep(2, 5)
             tab.close()
-            human_sleep(5, 10)
+            human_sleep(2, 3)
 
         else:
             tab.mouse.wheel(0, random.randint(500, 1000))
--- src/data/threads_scraper.py
+++ src/data/threads_scraper.py
@@ -3,6 +3,7 @@ import time
 import random
 import jmespath
 import os
+import sys
 import pandas as pd
 from browser_scraping_functions import human_sleep, random_mouse_move, random_mouse_hover, open_random_link
 from typing import Dict
@@ -35,14 +36,12 @@ def parse_thread(data: Dict) -> Dict:
         data,
     )
     
-    result["videos"] = list(set(result["videos"] or []))
+    result['videos'] = list(set(result['videos'] or []))
 
-    if result["reply_count"] and type(result["reply_count"]) != int:
-        result["reply_count"] = int(result["reply_count"].split(" ")[0])
+    if result['reply_count'] and type(result['reply_count']) != int:
+        result['reply_count'] = int(result['reply_count'].split(' ')[0])
         
-    result[
-        "url"
-    ] = f"https://www.threads.com/@{result['username']}/post/{result['code']}"
+    result['url'] = f"https://www.threads.com/@{result['username']}/post/{result['code']}"
     
     return result
 
@@ -50,21 +49,42 @@ def scrape_thread(page, url: str) -> dict:
     """Scrape Threads post and replies from a given URL"""
 
     page.goto(url)
-    page.wait_for_selector("[data-pressable-container=true]")
+    page.wait_for_selector('[data-pressable-container=true]')
+
+    """
+    Randomly selects a number of scrolls to perform before moving
+    on with scraping. Therefore, it should be noted as is, this will
+    not necessarily scrape every reply to very long threads. The
+    longer human sleep is to insure the page fully loads.
+    """
 
     max_scrolls = random.randint(9, 15)
     last_height = 0
 
-    human_sleep(10, 15)
+    human_sleep(5, 10)
 
     for scroll in range(max_scrolls):
+
+        """
+        Scrolls down through the page, getting all the links to
+        poster profiles and media and generates a random number
+        """
         
         page.mouse.wheel(0, 5000)
-        human_sleep(5, 15)
+        human_sleep(2, 7)
 
         chance = random.random()
         sel = Selector(text = page.content())
         links = sel.css('a[href^="/@"]::attr(href)').getall()
+
+        """
+        If the generated number is between 0.35 and 0.6, it will
+        randomly move the mouse. If the random number is
+        below 0.35 it will move the mouse and hover over an url.
+        Finally, if it's below 0.1, it will open the link in
+        a new tab and inspect it. This is designed to simulate
+        human mouse moving patterns and curiosity.
+        """
 
         if 0.35 < chance < 0.6:
             random_mouse_move(page)
@@ -76,11 +96,15 @@ def scrape_thread(page, url: str) -> dict:
             if chance < 0.10:
                 open_random_link(target, 'https://www.threads.com', context)
 
+        """
+        
+        """
+
         new_height = page.evaluate("() => document.body.scrollHeight")
 
-        if new_height == last_height:
+        if new_height == last_height or scroll == max_scrolls - 1:
             
-            human_sleep(5, 10)
+            human_sleep(2, 5)
             selector = Selector(page.content())
             hidden_datasets = selector.css('script[type="application/json"][data-sjs]::text').getall()
 
@@ -125,15 +149,21 @@ else:
 
 if __name__ == "__main__":
 
+    """
+    opens up the an auth_state to avoid login problems
+    and checks if there is an existing dataframe to save
+    results to, else it creates a new results dataframe.
+    """
+
     path_to_state = 'data/raw/threads_auth_state.json'
     
     if os.path.exists(path_to_state):
         auth_state = safe_loader(path_to_state)
 
     else:
-        print(f"❗❗❗Error. Browser save state not found at: {path_to_state}. "
-              f"Please check directory or run threads_get_login.py to "
-              f"generate a new save state.")
+       sys.exit(f"❗❗❗Error. Browser save state not found at: {path_to_state}. "
+                f"Please check directory or run threads_get_login.py to "
+                f"generate a new save state.")
 
     path_to_results = 'data/raw/threads_results.parquet'
 
@@ -142,34 +172,48 @@ if __name__ == "__main__":
 
     else:
         results = pd.DataFrame(columns = ['thread_id', 'parent_id', 'likes', 'user_id',
-                                          'content', 'published_on'])
+                                          'content', 'published_on', 'source'])
 
     old_results_length = len(results)
 
     base_url = 'https://www.threads.com'
 
     with sync_playwright() as pw:
+        """
+        opens up a new browser with the previous auth_state. The browser is not
+        headless because this can help avoid bot detection, although it is a bit
+        slower.
+        """
 
         browser = pw.chromium.launch(headless = False)
-        context = browser.new_context(viewport = {"width": 1920, "height": 1080},
+        context = browser.new_context(viewport = {'width': 1920, 'height': 1080},
                                       storage_state = auth_state)
         page = context.new_page()
 
         page.goto(base_url)
         page.wait_for_selector("[data-pressable-container=true]")
 
+        """
+        Scrolls down a bit on the threads feed before starting the search,
+        again, to simulate some human curiosity.
+        """
+
         page.mouse.wheel(0, 5000)
-        human_sleep(5, 10)
+        human_sleep(5, 7)
         random_mouse_move(page)
     
         for i, code in enumerate(codes):
+            """
+            this goes through the previously opened cash of threads post_ids
+            and extracts the content of the main post, and the content of replies.
+            It utilizes a cash of previously accessed posts in order to efficiently
+            skip previously visited pages.
+            """
             try:
 
                 if code not in cache:
                 
                     data = scrape_thread(page, base_url + code)
-                    
-                    parent_thread_code = code
 
                     df = pd.DataFrame({'thread_id' : [data['thread']['code']],
                                        'parent_id' : ['is_parent'],
@@ -184,7 +228,7 @@ if __name__ == "__main__":
                     for reply in data['replies']:
                         
                         df = pd.DataFrame({'thread_id' : [reply['code']],
-                                           'parent_id' : [parent_thread_code],
+                                           'parent_id' : [code],
                                            'likes' : [reply['like_count']],
                                            'user_id' : [reply['username']],
                                            'content' : [reply['text']],
@@ -192,6 +236,15 @@ if __name__ == "__main__":
                                            'source' : ['threads']})
 
                         results = pd.concat([results, df], ignore_index = True)
+
+                    """
+                    results are saved after iterating through a post, previously
+                    visited codes are removed from the codes set and added to the
+                    cache set. Both are saved. This is done at every step as a
+                    precaution because lost internet connections and throttled
+                    or blocked access to Threads may impact the collected data in
+                    memory.
+                    """
 
                     results.to_parquet(path_to_results)
 
@@ -203,14 +256,24 @@ if __name__ == "__main__":
             except Exception as e:
                 print(f"❗❗❗Failed to scrape post {code}: {str(e)}.")
 
-            # Short sleep after every request
-            human_sleep(3, 5)
 
-            # Take a longer break every 10 to 15 posts
+            """
+            This is a short sleep after each post, to simulate some human
+            randomness and to not access too much too fast, which can
+            flag the use of a bot. A longer break is performed every
+            10 to 15 posts to simulate human behavior.
+            """
+
+            human_sleep(3, 5)
             break_time = random.randint(10, 15)
             
             if (i + 1) % break_time == 0:
-                human_sleep(10, 20)
+                human_sleep(7, 10)
+
+    """
+    Simple check on the length of the dataset at the start and at the end,
+    due to the fact that internet and blocking can affect functionality.
+    """
 
     new_results_length = len(results)
 
@@ -219,5 +282,5 @@ if __name__ == "__main__":
               f"total of {new_results_length - old_results_length} comments were "
               f"retrieved.")
     else:
-        print("Failed to save script or no new results were retrieved, please check "
-              "to ensure that there aren't any bugs.")
+        print("Failed to save results or no new results were retrieved, please check "
+              "to ensure that there aren't any issues with the webscrapping.")
