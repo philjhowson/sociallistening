ad1ffbc3c0a57468a3f3e825385f5b06e3a346b5
philjhowson <philjhowson@outlook.com>
2025-06-25 18:06:55 +0200

    cleaning of src files and adding comments

--- src/formatting/data_formatting_pipeline.py
+++ src/formatting/data_formatting_pipeline.py
@@ -14,6 +14,10 @@ import spacy
 import html
 
 def get_country_code(row):
+    """
+    Takes a row from a dataset and converts lon/lat to a
+    country code. Returns np.nan if nothing is found.
+    """
     
     if row['lat'] != 0 and row['lon'] != 0:
         results = rg.search((row['lat'], row['lon']), mode = 1)
@@ -23,12 +27,21 @@ def get_country_code(row):
     
     return np.nan
 
+keywords1 = safe_loader('data/raw/keywords1.pkl')
+keywords2 = safe_loader('data/raw/keywords2.pkl')
+
 def preprocess_data(text):
+    """
+    Cleans up text by removing html code, lowercasing, removing trailing and leading
+    white space, normalizes a large number of items to a consistent single format.
+    keywords1 and keywords2 is loaded above and used here to maintain the privacy of
+    the words to be normalized because this was not built as part of a personal project.
+    """
 
     text = html.unescape(text)   
     text = text.lower().strip()
     text = text.replace('“', "\"").replace('”', "\"").replace('‘', "'").replace('’', "'")
-    text = text.replace('apple care', 'applecare').replace('samsung care', 'samsungcare')
+    text = text.replace(keywords1[0], keywords1[1]).replace(keywords2[0], keywords2[1])
     text = re.sub(r'？', '?', text)
     text = re.sub(r'¿', '?', text)
     text = re.sub(r'！', '!', text)
@@ -47,16 +60,26 @@ def preprocess_data(text):
 
     return text
 
+"""
+Updates the list of punctuations to include other non-English
+punctuation that is commonly used.
+"""
+
 punct_to_remove = set(string.punctuation)
 punct_to_remove -= {'!', '?', "'"}
 punct_to_remove.update(['«', '»', '„', '“', '”', '‹', '›',
-                        '–', '—', '…', '‚', '‐', '′', '″',
+                        '–', '—', '‚', '‐', '′', '″',
                         '′′', '«', '»', '´', '`', '¸', '·',
                         '•', '。', '、', '，', '．', '：', '；',
                         '（', '）', '【', '】', '『', '』', '〝',
                         '〞'])
 
 def remove_punctuation(text, whitelist = None):
+    """
+    Removes punctuation. Splits a text up into chunks and removes all
+    punctuation, although '...' for example is kept as it can be useful
+    for many sentiment analysis systems and embeddings.
+    """
 
     cleaned_words = []
 
@@ -87,6 +110,9 @@ def remove_punctuation(text, whitelist = None):
     return re.sub(r'\s+', ' ', text)
     
 def detect_language(text):
+    """
+    Uses langdetect to attempt to identify the language.
+    """
     
     try:
         return detect(text)
@@ -94,8 +120,11 @@ def detect_language(text):
     except LangDetectException:
         return 'unknown'
 
-def reduce_repeats(text):
-    return re.sub(r'(.)\1{2,}', r'\1\1', text)
+"""
+Loads spacy for English. More spacy models can be loaded
+using a spacy_codes.pkl that should be set up to look like a
+dictionary.
+"""
 
 try:
     #spacy_codes = safe_loader('data/raw/spacy_codes.pkl')
@@ -106,13 +135,22 @@ except Exception as e:
     
 spacy_models = {}
 
+"""
+Loads all the models specified in spacy_codes.
+"""
+
 if spacy_codes:
     for lang in spacy_codes:
         model_name = spacy_codes[lang]
         spacy_models[lang] = spacy.load(model_name)
 
-
 def remove_stopwords(text, lang, custom = None):
+    """
+    Removes english stopwords in spacy (or other languages if modified above)
+    and utilizes a custom set of stopwords for other languages to remove
+    other stopwords. This was largely done because spacy does not have strong
+    enough models to support all the languages in my dataset.
+    """
     
     stopwords = set()
 
@@ -136,12 +174,20 @@ def remove_stopwords(text, lang, custom = None):
     return ' '.join(filtered_words)
 
 def has_enough_char(text, min_char = 3):
+    """
+    Determines if a string has enough characters to be kept. If it has 3
+    characters or less it will be marked for removal.
+    """
     text = re.sub(r'\s+', '', text)
     return len(regex.findall(r'\p{L}', str(text))) >= min_char
     
 _stanza_pipelines = {}
 
 def get_stanza_pipeline(lang):
+    """
+    Gets all the relevant stanza pipelines for lemmatization. This was done largely
+    because spacy did not have all the required and relevant language support.
+    """
 
     if lang not in _stanza_pipelines:
         try:
@@ -155,6 +201,10 @@ def get_stanza_pipeline(lang):
     return _stanza_pipelines.get(lang)
 
 def lemmatize(text, lang):
+    """
+    Lemmatizes each word using either spacy (if available) or
+    stanza.
+    """
 
     spacy_model = spacy_models.get(lang)
         
@@ -181,6 +231,13 @@ def pipeline(data, input_col, iso_codes = None, custom_stops = None, whitelist =
              spacy = None, geolocation = True, detect_lang = True,
              preprocessing = True, remove_stops = True, lemma = True,
              punctuation = True, characters = True):
+    
+    """
+    Pass a whole df and get the processed df back. This performs geolocation,
+    language detection, preprocessing functions, stop word removal, lemmatization,
+    and finally, checks if there are enough characters and removes rows where
+    text is too short to reliably find any information.
+    """
 
     df = data.copy()
 
--- src/formatting/format_data.py
+++ src/formatting/format_data.py
@@ -13,6 +13,11 @@ import os
 path_to_processed = 'data/processed'
 
 def preprocess_youtube():
+    """
+    Loads in the youtube data and the relevant iso_codes, stopwords, and whitelists
+    for processed. Runs the custom pipeline() function on both the comments and
+    video title.
+    """
 
     data = pd.read_parquet(f"data/raw/youtube_results.parquet")
 
@@ -22,11 +27,26 @@ def preprocess_youtube():
     whitelist = shared_functions.safe_loader('data/raw/whitelisted_characters.pkl')
 
     data = pipeline(data, 'comment_text', iso_codes = iso_codes, custom_stops = custom_stops, whitelist = whitelist)
-    data = pipeline(data, 'video_title', iso_codes = iso_codes, custom_stops = custom_stops, whitelist = whitelist)
+
+    """
+    Because there are way more rows than unique 'video_title' rows, I
+    decided to speed up the processing by only processing the unique titles
+    and then mapping them to a new column in data to speed the process up.
+    """
+
+    titles = data.drop_duplicates(subset = ['video_title']).copy()
+    titles = pipeline(titles, 'video_title', iso_codes = iso_codes, custom_stops = custom_stops, whitelist = whitelist)
+    title_map = dict(zip(titles['video_title'], titles['cleaned_video_title']))
+    data['cleaned_video_title'] = data['video_title'].map(title_map)
 
     data.to_parquet(f"{path_to_processed}/cleaned_youtube_results.parquet")
 
 def preprocess_reddit():
+    """
+    Loads in the reddit data, custom stops, whitelists, and a file that maps each
+    subreddit where the geographical region is obvious from the name to that region,
+    e.g., r/France --> 'Western Europe'.
+    """
     
     data = pd.read_parquet(f"data/raw/reddit_results.parquet")
 
@@ -36,11 +56,23 @@ def preprocess_reddit():
     whitelist = shared_functions.safe_loader('data/raw/whitelisted_characters.pkl')
     subreddits_to_region = shared_functions.safe_loader('data/raw/subreddit_to_region.pkl')
 
+    """
+    The original formatting has a separate column for original post and replies, here,
+    I stack the original posts onto the comment_body column along with relevant metrics,
+    like data and likes, in order to have one column with all the content to be cleaned.
+    """
+
     original_posts = data.drop_duplicates(subset = 'post_body')
     original_posts.loc[:, 'comment_body'] = original_posts['post_body']
     original_posts.loc[:, 'comment_score'] = original_posts['post_score']
     original_posts.loc[:, 'comment_created_utc'] = original_posts['post_created_utc']
     data = pd.concat([data, original_posts], ignore_index = True)
+
+    """
+    Runs the pipeline without geolocation since this uses lat/lon that are not
+    present in the reddit data. Then it maps the subreddits to regions that can be
+    and the rest to 'Unknown'. Drops content that is not of interest.
+    """
 
     data = pipeline(data, 'comment_body', custom_stops = custom_stops, whitelist = whitelist,
                     geolocation = False)
@@ -54,6 +86,10 @@ def preprocess_reddit():
     data.to_parquet(f"{path_to_processed}/cleaned_reddit_results.parquet")
 
 def preprocess_threads():
+    """
+    Scraping resulted in a few NA values, fortunately not many, so
+    those are filled and columns with NA dates are dropped.
+    """
 
     data = pd.read_parquet(f"data/raw/threads_results.parquet")
     data['content'] = data['content'].fillna('None')
@@ -61,6 +97,12 @@ def preprocess_threads():
     data.dropna(subset = ['published_on'], inplace = True)
 
     print('Beginning preprocessing...')
+
+    """
+    Loads in the relevant custom_stops and whitelist and then runs the pipeline
+    without the geolocation function because it is specialized for lat/lon that
+    is only available from youtube. Drops a few irrelevant columns and saves.
+    """
 
     custom_stops = shared_functions.safe_loader('data/raw/custom_stopwords.pkl')
     whitelist = shared_functions.safe_loader('data/raw/whitelisted_characters.pkl')
@@ -74,9 +116,19 @@ def preprocess_threads():
     data.to_parquet(f"{path_to_processed}/cleaned_threads_results.parquet")  
 
 def create_masterdata():
+    """
+    This function loads in all the datasets to create one parquet file that has
+    all the masterdata in it.
+    """
 
     youtube = pd.read_parquet('data/processed/cleaned_youtube_results.parquet')
 
+    """
+    Countries are converted to regions here e.g., Brazil --> South America,
+    converts the date-time to a consistent format and marks the columns to keep
+    and drops the rest. Renames columns so that they will all be consistent across
+    datasets.
+    """
     country_to_region = shared_functions.safe_loader('data/processed/youtube_countries_to_region.pkl')
     youtube['region'] = youtube['geolocation'].map(country_to_region).fillna('Unknown')
     youtube['comment_time'] = pd.to_datetime(youtube['comment_time'], errors = 'coerce').dt.date
@@ -93,8 +145,11 @@ def create_masterdata():
 
     reddit = pd.read_parquet('data/processed/cleaned_reddit_results.parquet')
 
-    country_to_region = shared_functions.safe_loader('data/processed/reddit_countries_to_region.pkl')
-    reddit['region'] = reddit['subreddit'].map(country_to_region).fillna('Unknown')
+    """
+    Adds an empty hashtags column, formats the data-time, and keeps only columns of interest.
+    Renames columns for consistency.
+    """
+
     reddit['hashtags'] = [[] for i in range(len(reddit))]
     reddit['comment_created_utc'] = pd.to_datetime(reddit['comment_created_utc'])
     columns_to_keep = ['post_title', 'hashtags', 'comment_body', 'comment_created_utc',
@@ -107,6 +162,11 @@ def create_masterdata():
     reddit.rename(renamed_columns, axis = 1, inplace = True)
 
     threads = pd.read_parquet(f"{path_to_processed}/cleaned_threads_results.parquet")
+    """
+    Regional data isn't available for threads, so the whole column is 'Unknown', an
+    empty hashtags column is created, there is no title so an empty 'title' column
+    is created and the data-time is formatted. Renames columns for consistency.
+    """
     threads['region'] = 'Unknown'
     threads['hashtags'] = [[] for i in range(len(threads))]
     threads['title'] = 'None'
@@ -118,17 +178,35 @@ def create_masterdata():
 
     data = pd.concat([youtube, reddit, threads], ignore_index = True)
     data.reset_index(drop = True, inplace = True)
+    """
+    There were a few likes errors in the data, so I coerce them to numeric,
+    clip the lower bound to 0, fill any NAs as 0, and do np.log1p to make
+    a log_likes column for later scoring. Log likes is used to reduce the
+    scoring impact of incredibly viral posts. Converts the date-time to
+    just the year and month of post. This makes time analysis easier because
+    more general trends are desired, and further anonymizes the data, complying
+    with GDPR and DSGVO regulations.
+    """
     data['likes'] = pd.to_numeric(data['likes'], errors = 'coerce')
     data['likes'] = data['likes'].clip(lower = 0)
     data['likes'] = data['likes'].fillna(0).astype(int)
     data['log_likes'] = np.log1p(data['likes'])
     data['month_year'] = data['date'].dt.to_period('M').astype(str)
 
+    """
+    Just organizes the dataframe columns the way I wanted them and then
+    saves the data.
+    """
     data = data[['title', 'text', 'likes', 'source', 'region', 'date', 'language', 'hashtags', 'cleaned_text']]
 
     data.to_parquet('data/processed/cleaned_masterdata.parquet')
 
 def embeddings():
+    """
+    This is for making the embeddings that are used in cosine_similarity. I use
+    'paraphrase-multilingual-MiniLM-L12-v2' because the dataset contains over 50
+    languages and is extremely large so translation into English is not possible. 
+    """
 
     data = pd.read_parquet(f"{path_to_processed}/cleaned_masterdata.parquet")
 
@@ -137,6 +215,13 @@ def embeddings():
     preembedded = shared_functions.safe_loader('data/processed/custom_phrases_for_embeddeding.pkl')
     
     print('Generating topic embeddings...')
+    
+    """
+    I made custom topics/themes that I described in sentences and loaded them above.
+    Those themes are then embedded and the average value is an average value vector
+    is create for each theme. For the cosine similarity function later, comment vectors
+    will be compared against these themes for similarity.
+    """
     
     topics = {}
 
@@ -150,17 +235,33 @@ def embeddings():
 
     print('Generating text embeddings...')
 
+    """
+    Embeddings are then generated for all the comments and saved using np.savez_compressed.
+    I used this because it does save some space compared to .pkl and the data is quite
+    plentiful.
+    """
+
     embeddings = model.encode(data['cleaned_text'].to_list())
     embeddings = normalize(embeddings)
     np.savez_compressed(f"{path_to_processed}/masterdata_embeddings.npz", embeddings = embeddings)
 
 def cosine_similarity():
+    """
+    Loads in the data and the previously made embeddings.
+    """
 
     data = pd.read_parquet(f"{path_to_processed}/cleaned_masterdata.parquet")
 
     topic_embeddings = shared_functions.safe_loader('data/processed/custom_embeddings.pkl')
     comment_embeddings_archive = np.load(f"{path_to_processed}/masterdata_embeddings.npz")
     comment_embeddings = comment_embeddings_archive['embeddings']
+
+    """
+    Here I create variables to keep track of the names of every low_similarity_column
+    that gets generated and each similarity column that gets generated. I set the
+    general threshold to 0.5, such that any comment with less than 0.5 similarity
+    score gets marked for removal.
+    """
 
     low_similarity_columns = []
     similarity_columns = []
@@ -173,9 +274,17 @@ def cosine_similarity():
         low_similarity = f"low_{topic}_similarity"
         low_similarity_columns.append(low_similarity)
         values = topic_embeddings.get(topic)
-
+        """
+        Because I normalized the vectors for the embeddings, the cosine similarity
+        is just the dot product, so I used it here.
+        """
         data[similarity] = [np.dot(values, comment_embeddings[row]) for row in range(len(data))]
         data[low_similarity] = data[similarity] < threshold
+
+        """
+        Plots the distributions of similarities scores for each topic for later inspection
+        and possible adjustment of thresholding.
+        """
 
         fig = plt.figure(figsize = (10, 10))
 
@@ -189,11 +298,23 @@ def cosine_similarity():
 
         plt.savefig(f"images/cosine_similarities_{topic}.png")
 
+    """
+    This finds every row that has low similarity for all the topics and marks it.
+    Then I remove all those rows from the dataset and drop the columns used to find
+    similarities.
+    """
     low_similarity = (data[low_similarity_columns] >= threshold).all(axis = 1)
     before = len(data)
     reduced_data = data[~low_similarity].copy()
     reduced_data.drop(columns = low_similarity_columns + similarity_columns, inplace = True)
 
+    """
+    Here I have a few keywords I look for in the text and I use this just to be
+    on the safe side, incase the similarity scores still filtered them out.
+    I use a set to compare because the lookup time for each value is O(1).
+    It exits the comparison for each item as soon as any keyword is found, speeding
+    the search up a little.
+    """
     keywords = shared_functions.safe_loader('data/processed/custom_keywords.pkl')
 
     rows_to_keep = []
@@ -205,6 +326,13 @@ def cosine_similarity():
             continue
 
     keyword_matched_data = data.iloc[rows_to_keep]
+
+    """
+    I keep the rows that meet the keywords, then I combine with the cosine reduced
+    dataset. Because there is likely some overlap and I don't want duplicates, I
+    drop any duplicate indices. The data is saved as is the low_similarity vector that
+    was used to filter comments.
+    """
 
     final_reduced_data = pd.concat([reduced_data, keyword_matched_data])
     final_reduced_data = final_reduced_data[~final_reduced_data.index.duplicated(keep = 'first')]
@@ -240,8 +368,16 @@ def format_data(function):
             cosine_similarity()
 
 if __name__ == '__main__':
+    """
+    Simple argument parser.
+        --function: youtube, reddit, or threads to preprocess those datasets.
+                    master to create the masterdata from the preprocessed datasets.
+                    embeddings to create embeddings, similarity to use
+                    cosine similarity to filter comments.
+                    all to run everything.
+    """
     parser = argparse.ArgumentParser(description = 'prepare data for analysis')
-    parser.add_argument('--function', default = 'all', help = 'Default: "all", Options: "preprocess" to prepare data, "embeddings" to creating embeddings, "similarity" to do cosine similarity for comments, "all" to do all steps.')
+    parser.add_argument('--function', default = 'all', help = 'Default: "all", Options: "youtube", "reddit", or "threads" to prepare those datasets, "master" to create masterdata set from all datasets, "embeddings" to creating embeddings, "similarity" to do cosine similarity for comments, "all" to do all steps.')
 
     arg = parser.parse_args()
 
